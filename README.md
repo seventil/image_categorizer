89a5f1af41469132e26b88f4fb7e2289a3553b52
seventil <ales.kutepov@gmail.com>
2024-01-20 16:04:37 +0300

    Added classes for evaluated images and its containers, that allow to store and read info about images from json files. Updated README files to accomodate to this structure.

--- README.md
+++ README.md
@@ -11,3 +11,48 @@ for each category. For example, one category is landscape with ratings on compos
 the other is anatomy with ratings on complexity. Some categories may be exclusive - that means
 they'll get their separate folders. For example, ballet may be exlusive and include photographic,
 anatomy nested categories.
+
+On top of evaluations listed in a schema, images are evaluated on category scales as well.
+Photography categry with concept subcategory may include evaulations on color and texture
+which gives 4 evaulations total: photography, concept, color, texture.
+
+Here is a view of how json databank describing images and their evaulations are storred compared to
+how the actual images are stored. A, B, C... indexes stand to store no more than 1000 images in 
+one folder and to differentiate between how they're evaluated. If no evaluation ranges are present 
+yet, it's only two: 1/2. The higher the mark - the better the parameter. When working through
+storred images it's important to do relative evaluations by comparison. If 1/2 is not enough and
+you'd want something in between: 2 shifts to 3, and 2 becomes empty, now you can restructure 
+1 into 1,2; 3 into 2,3 and after that compare whatever you have in 2 with each other with new shift 
+3->4, 2->2,3.
+For subcategories naming goes the following way: <category mark>_<subcategory mark>_<literal index>
+
+- databank
+    - category1 folder
+        - 1_A.json
+        - 1_B.json
+        - 2_A.json
+        - category1_1_subcategory1 folder
+            - 1_1_A.json
+
+- outputs
+    - category1 folder
+        - 1_A folder
+            - 1_A img1
+            ...
+            - 1_A img1000
+        - 1_B folder
+            - 1_B img1
+            ...
+            - 1_B img1000
+        - 2_A folder
+            - 2_A img1
+            ...
+            - 2_A img1000
+        - category1_subcategory1 folder
+            - 1_1_A folder
+                - 1_1_A img1
+                ...
+                - 1_1_A img1000
+
+## Project setup
+install requirements
---
+++ image_nodes.py
@@ -0,0 +1,233 @@
+import json
+import os
+from typing import Dict, List
+
+DEFAULT_PATH = "databank"
+DEFAULT_ENCODING = "utf-8"
+JSON_INDENT = 4
+MAX_ITEMS_PER_NODE = 1000
+STORAGE_FORMAT = "json"
+
+
+class DataBankSchema:
+    """Describes the names of json nodes for image info"""
+
+    storage_path = "Path"
+    categories = "Categories"
+    evals = "Evals"
+
+
+class EvaluatedPic:
+    """Encapsulates evaluations for an image with info on where it is stored.
+
+    Attributes:
+        storage_path (str): describes where the image is storred.
+        categories (List[str]): categories (from schema) that the image fits.
+        evals (Dict[str, int]): evaluations that where set for the image.
+        resize (bool): if the image should be resized to save storage space.
+            Defaults to True. Should be set to False if high-definition texture
+            is important.
+    """
+
+    def __init__(
+        self,
+        storage_path: str,
+        categories: List[str] = None,
+        evals: Dict[str, int] = None,
+        resize: bool = True,
+    ) -> None:
+        """Initialize the object with all attributes.
+
+        Args:
+            storage_path (str): describes where the image is storred.
+            categories (List[str]): categories (from schema) that the image fits.
+            evals (Dict[str, int]): evaluations that where set for the image.
+            resize (bool): if the image should be resized to save storage space.
+                Defaults to True. Should be set to False if high-definition texture
+                is important.
+        """
+        self.storage_path = storage_path
+        if categories is None:
+            self.categories = []
+        else:
+            self.categories = categories
+        if evals is None:
+            self.evals = {}
+        else:
+            self.evals = evals
+        self.resize = resize
+
+    def add_category(self, category: str) -> None:
+        """Add a category that the image fits into.
+
+        Args:
+            category (str): name of the category from the schema file.
+        """
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def evaluate(self, category: str, mark: int) -> None:
+        """Add or change an evaluation for the image.
+
+        Args:
+            category (str): name of the eval from the schema file.
+            mark (int): relative evaluation mark for the image in the specified
+                category.
+        """
+        self.evals[category] = mark
+
+
+class ImageStorageNode:
+    """A node to store evaluated image objects differentiated by categories.
+
+    Attributes:
+        rank (int | None): category mark of the images contained in the node.
+        bucket (str | None): bucket that splits the images by MAX_ITEMS_PER_NODE.
+        images (List[EvaluatedPic]): evaluated images.
+    """
+
+    def __init__(
+        self,
+        rank: int | None,
+        bucket: str | None,
+        evaluated_pics: List[dict] | None = None,
+    ) -> None:
+        """Instantiate the node with possible rank and json data for pics.
+
+        Args:
+            rank (int | None): mark for the images in the node for respective category.
+            bucket (str | None): bucket that splits the images by MAX_ITEMS_PER_NODE.
+            evaluated_pics (List[dict] | None): json data for pics. Defaults to None.
+        """
+        self.rank = rank
+        self.bucket = bucket
+
+        if evaluated_pics is None:
+            self.images: List[EvaluatedPic] = []
+        else:
+            self.images: List[EvaluatedPic] = [
+                EvaluatedPic(
+                    storage_path=pic.get(DataBankSchema.storage_path),
+                    categories=pic.get(DataBankSchema.categories),
+                    evals=pic.get(DataBankSchema.evals),
+                )
+                for pic in evaluated_pics
+            ]
+
+    def add_image(self, image: EvaluatedPic) -> bool:
+        """Add image object to the node.
+
+        Args:
+            image (EvaluatedPic): image to add.
+        Returns:
+            bool: if the image was added.
+        """
+        if len(self.images > MAX_ITEMS_PER_NODE):
+            return False
+        self.images.append(image)
+        return True
+
+    def pop_image(self) -> EvaluatedPic:
+        """Pop image from the node."""
+        return self.images.pop()
+
+
+class ImageNodesHolder:
+    """Parent for image nodes that maps nodes to their respective categories.
+
+    Attributes:
+        image_nodes (Dict[str, ImageStorageNode]): mapping of nodes to categories.
+    """
+
+    def __init__(self) -> None:
+        """Initialize node container."""
+        self.image_nodes: Dict[str, ImageStorageNode] = {}
+
+    def find_node(self, image: EvaluatedPic) -> ImageStorageNode:
+        """Returns node that fits the image object based on its attributes.
+
+        Takes into account primary category, all present subcategories and their
+        respective evaluations. If several nodes match the criteria, finds the
+        one that has empty space for the image.
+
+        Args:
+            image (EvaluatedPic): image the node is searched for.
+        Returns:
+            ImageStorageNode fitting the criteria.
+        """
+        raise NotImplementedError
+
+    def _filter_json_files(self, files: List[str]) -> List[str]:
+        """Filters json files from the list of files.
+
+        Args:
+            files (List[str]): files with different formats.
+        Returns:
+            List[str] of files with json format.
+        """
+        filtered_files = []
+        for file in files:
+            if file.split(".")[-1].lower() == STORAGE_FORMAT:
+                filtered_files.append(file)
+
+        return filtered_files
+
+    def read(self, path: str = DEFAULT_PATH):
+        """Read the databank folder.
+
+        The root must contain folder structure fitting categories and json files
+        with lists of evaluated images data.
+
+        Args:
+            path (str): path to the root folder. Defaults to DEFAULT_PATH.
+        """
+        for folder, _, files in os.walk(path):
+            if len(files) == 0:
+                continue
+            rel_path = os.path.relpath(folder, start=path)
+            files = self._filter_json_files(files)
+
+            nodes = []
+            for file in files:
+                full_path = os.path.join(folder, file)
+                with open(full_path, "r", encoding=DEFAULT_ENCODING) as fstream:
+                    eval_pics_json_data = json.load(fstream)
+                node_name = os.path.basename(full_path).split(".")[0]
+                node_rank, node_bucket = node_name.split("_")
+                nodes.append(
+                    ImageStorageNode(
+                        rank=int(node_rank),
+                        bucket=node_bucket,
+                        evaluated_pics=eval_pics_json_data,
+                    )
+                )
+            self.image_nodes[rel_path] = nodes
+
+    def save(self, root_path: str = DEFAULT_PATH):
+        """Save node structure to the databank folder.
+
+        The root contains folder structure fitting categories and json files
+        with lists of evaluated images data.
+
+        Args:
+            root_path (str): path to the root folder. Defaults to DEFAULT_PATH.
+        """
+        for path, image_nodes in self.image_nodes.items():
+            for node in image_nodes:
+                evaluated_images = []
+                for img in node.images:
+                    evaluated_img_json = {
+                        DataBankSchema.storage_path: img.storage_path,
+                        DataBankSchema.categories: img.categories,
+                        DataBankSchema.evals: img.evals,
+                    }
+                    evaluated_images.append(evaluated_img_json)
+                output_name = f"{node.rank}_{node.bucket}.{STORAGE_FORMAT}"
+                output_path = os.path.join(root_path, path)
+                os.makedirs(output_path, exist_ok=True)
+                with open(
+                    os.path.join(output_path, output_name),
+                    "w",
+                    encoding=DEFAULT_ENCODING,
+                ) as fstream:
+                    json.dump(evaluated_images, fstream, indent=JSON_INDENT)
