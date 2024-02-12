07a58204157a8f55ac5c10f2d27d76b1eda71d11
seventil <ales.kutepov@gmail.com>
2024-02-12 18:07:09 +0300

    fixed image duplication in nodes after every save in ImageNodesHolder.post_pic()

--- image_nodes.py
+++ image_nodes.py
@@ -8,9 +8,11 @@ MAX_ITEMS_PER_NODE = 1000
 
 IMAGE_FILE_FORMATS = ["jpg", "jpeg", "png", "webp"]
 
-
+type CategoryHierarchyPath = str
 type SiblingNodes = list[ImageStorageNode]
-type NodesPathMap = dict[str, SiblingNodes]
+type NodesPathMap = dict[CategoryHierarchyPath, SiblingNodes]
+type NodeName = str
+type NodeRef = tuple[CategoryHierarchyPath, NodeName]
 type NodePics = list[EvaluatedPic]
 type SortedRanks = tuple[int, ...]
 
@@ -40,9 +42,9 @@ class EvaluatedPic:
             storage_path (str): describes where the image is storred.
             categories (Categories | None): categories (from schema) that the image fits.
             evals (Evaluations | None): evaluations that where set for the image.
-            resize (bool): if the image should be resized to save storage space.
-                Defaults to True. Should be set to False if high-definition texture
-                is important.
+            resize (bool): if the image should be resized (on physical save) 
+                to save storage space. Defaults to True. Should be set to False 
+                if high-definition texture is important.
         """
         self.storage_path = storage_path
         if categories is None:
@@ -54,6 +56,8 @@ class EvaluatedPic:
         else:
             self.__evals = evals
         self.resize = resize
+
+        self.__current_node_ref: ImageStorageNode = None
 
     def add_category(self, category: str, category_priority: PrioritizedCategories) -> None:
         """Add a category that the image fits into.
@@ -105,6 +109,14 @@ class EvaluatedPic:
     @property
     def sorted_ranks(self) -> SortedRanks:
         return (self.__evals[rank] for rank in self.categories)
+    
+    @property
+    def node_ref(self) -> "ImageStorageNode":
+        return self.__current_node_ref
+    
+    @node_ref.setter
+    def node_ref(self, new_node: "ImageStorageNode"):
+        self.__current_node_ref = new_node
 
 
 class ImageStorageNode:
@@ -113,14 +125,14 @@ class ImageStorageNode:
     Attributes:
         ranks (SortedRanks | None): category mark of the images contained in the node.
         bucket (str | None): bucket that splits the images by MAX_ITEMS_PER_NODE.
-        name (str | None): name of the node, containing info on it's ranks for
+        name (NodeName | None): name of the node, containing info on it's ranks for
             categories and subcategories and its bucket.
         images (SiblingNodes): evaluated images.
     """
 
     def __init__(
         self,
-        name: str | None = None,
+        name: NodeName | None = None,
         ranks: SortedRanks | None = None,
         bucket: str | None = None,
         evaluated_pics: list[dict] | None = None,
@@ -134,7 +146,7 @@ class ImageStorageNode:
             evaluated_pics (list[dict] | None): json data for pics. Defaults to None.
         """
         if name is not None:
-            self.__name: str = name
+            self.__name: NodeName = name
             name = name.split("_")
             self.bucket: str = name.pop(-1)
             self.ranks: SortedRanks = tuple(map(int, name))
@@ -156,7 +168,7 @@ class ImageStorageNode:
             ]
 
     @property
-    def name(self) -> str:
+    def name(self) -> NodeName:
         """The node name property.
 
         Contains info on it's ranks for categories and subcategories and its bucket.
@@ -177,23 +189,25 @@ class ImageStorageNode:
         """
         if len(self.images > MAX_ITEMS_PER_NODE):
             return False
+        image.node_ref.pop_image(image)
         self.images.append(image)
+        image.node_ref = self
         return True
 
-    def pop_image(self, index: int = -1) -> EvaluatedPic:
+    def pop_image(self, pic: EvaluatedPic) -> None:
         """Pop image from the node."""
-        return self.images.pop(index)
+        index = self.__index_image(pic)
+        self.images.pop(index)
     
-    def index_image(self, pic: EvaluatedPic) -> int | None:
+    def __index_image(self, pic: EvaluatedPic) -> int | None:
         """Looks for an image in NodePics images list.
 
         Returns:
             int: index if image is present.
-            None: if image was not found in NodePics images list.
         """
         if pic in self.images:
             return self.images.index(pic)
-        return None
+        raise ValueError("Evaluated Pic object was not found in this Node.")
 
 
 class ImageNodesHolder:
@@ -235,8 +249,6 @@ class ImageNodesHolder:
         If a fitting node does not exist, creates it.
 
         Asserts categories of the EvaluatedPic are sorted as per the schema.
-        Asserts that the image is not present in the nodes, otherwise creates
-            a duplicate.
 
         Args:
             image (EvaluatedPic): image the node is searched for.
@@ -252,6 +264,7 @@ class ImageNodesHolder:
             for node in sibling_nodes
             if node.ranks == image.sorted_ranks
         ]
+
         for node in fitting_mark_nodes:
             if node.add_image(image):
                 return
